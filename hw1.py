6ccfa299dc298a84e1cfa3fb898d21d6eecd6d5c
Lior Sukman <lior.sukman@gmail.com>
2020-11-18 11:52:11 +0200

    added my parts to the notebook and renamed

--- hw1.py
+++ hw1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from naive_knn import KNearestNeighbors
 
@@ -13,19 +14,19 @@ def calc_mce(pred, gold):
             counter += 1
     return counter / len(pred)
 
-def sklearn_knn(X_train, y_train, X_test, y_test, ks):
-    mces = []
-    for k in ks:
-        neigh = KNeighborsClassifier(n_neighbors = k)
-        neigh.fit(X_train, y_train)
-        pred = neigh.predict(X_test)
-        mce = calc_mce(pred, y_test)
-        mces.append(mce)
-    return mces
+def sklearn_knn(X_train, y_train, X_test, y_test, k):
+    neigh = KNeighborsClassifier(n_neighbors = k) #, weights = lambda x: np.where(x == 0, 1, 1 / (x ** 2))) # this was used as sanity check for part 7
+    neigh.fit(X_train, y_train)
+    pred = neigh.predict(X_test)
+    mce = calc_mce(pred, y_test)
+    print('For k = %d the misclassification rate of sklearn KNN is: %.3f' % (k, mce))
+    return mce
 
 def part_4(X_train, y_train, X_test, y_test):
     ks = [1, 5, 10]
-    mces = sklearn_knn(X_train, y_train, X_test, y_test, ks)
+    mces = []
+    for k in ks:
+        mces.append(sklearn_knn(X_train, y_train, X_test, y_test, k))
     ks = [1/k for k in ks]
     plt.plot(ks, mces)
     plt.scatter(ks, mces)
@@ -34,12 +35,32 @@ def part_4(X_train, y_train, X_test, y_test):
     plt.title('misclasification error vs 1/k')
     plt.show()
 
-def part_5(X_train, y_train, X_test, y_test):
-    neigh = KNearestNeighbors()
+kinds = ['naive', 'weighted']
+
+def part_5(X_train, y_train, X_test, y_test, k, kind = 'naive'):
+    assert kind in kinds
+    neigh = KNearestNeighbors(n_neighbors = k)
     neigh.fit(X_train, y_train)
-    pred = neigh.predict(X_test)
+    if kind == 'naive':
+        pred = neigh.predict(X_test)
+    else:
+        pred = neigh.predict_weighted(X_test)
     mce = calc_mce(pred, y_test)
-    print('The misclassification rate for the naive implementation of KNN is:', mce)
+    print('For k = %d the misclassification rate for %s KNN is: %.3f' % (k, kind, mce))
+    return mce
+
+def part_7(X_train, y_train, X_test, y_test):
+    ks = [1, 5, 10]
+    mces = []
+    for k in ks:
+        mces.append(part_5(X_train, y_train, X_test, y_test, k, kind = 'weighted'))
+    ks = [1/k for k in ks]
+    plt.plot(ks, mces)
+    plt.scatter(ks, mces)
+    plt.xlabel('1/k')
+    plt.ylabel('misclassification error')
+    plt.title('misclasification error vs 1/k')
+    plt.show()
 
 if __name__ == "__main__":
     dataset_dir = "https://raw.githubusercontent.com/probml/pmtkdata/master/knnClassify3c/"
@@ -58,7 +79,11 @@ if __name__ == "__main__":
     X_test = test_dataset.iloc[:, :-1].values
     y_test = test_dataset.iloc[:, -1].values
 
-    part_4(X_train, y_train, X_test, y_test)
+    #part_4(X_train, y_train, X_test, y_test)
     
-    part_5(X_train, y_train, X_test, y_test)
+    #part_5(X_train, y_train, X_test, y_test, 1)
+
+    #part 6 is only the implementation in the naive_knn.py file
+
+    part_7(X_train, y_train, X_test, y_test)
     
--- naive_knn.py
+++ naive_knn.py
@@ -20,27 +20,44 @@ class KNearestNeighbors:
         """
         Returns an array of the classes of the *k* nearest neighbors.
         """
-        distances = np.sqrt(np.sum((self.X_train - observation)**2, axis=1))
+        distances = np.sqrt(np.sum((self.X_train - observation) ** 2, axis = 1))
 
         # Create an array of training set indices ordered by their
         # distance from the current observation
-        indices = np.argsort(distances, axis=0)
+        indices = np.argsort(distances, axis = 0)
 
         selected_indices = indices[:self.k]
-        return self.y_train[selected_indices]
+        return self.y_train[selected_indices], distances[selected_indices]
 
     def estimate_class(self, observation: np.ndarray) -> int:
         """
         Estimates to which class a given row (*observation*) belongs.
         """
-        neighbor_classes = self.get_neighbor_classes(observation)
-        classes, counts = np.unique(neighbor_classes, return_counts=True)
+        neighbor_classes, _ = self.get_neighbor_classes(observation)
+        classes, counts = np.unique(neighbor_classes, return_counts = True)
         return classes[np.argmax(counts)]
 
-    # Replace with `mode()`
+    def estimate_class_weighted(self, observation: np.ndarray) -> int:
+        """
+        Estimates to which class a given row (*observation*) belongs with the weighted prediction.
+        """
+        neighbor_classes, distances = self.get_neighbor_classes(observation)
+        weights =  np.where(distances == 0, 1, 1 / (distances ** 2))
+        classes = np.unique(neighbor_classes)
+        best_c, best_w = 0, 0
+        for c in np.nditer(classes):
+            w = np.sum(weights[np.nonzero(neighbor_classes == c)])
+            best_w, best_c = (w, c) if w > best_w else (best_w, best_c)
+        return best_c
 
     def predict(self, X: np.ndarray):
         """
         Apply k-NN estimation for each row in a given dataset.
         """
         return np.apply_along_axis(self.estimate_class, 1, X)
+
+    def predict_weighted(self, X: np.ndarray):
+        """
+        Apply k-NN estimation for each row in a given dataset with weighted predictions.
+        """
+        return np.apply_along_axis(self.estimate_class_weighted, 1, X)
